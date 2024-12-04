7ca86a28551f3ab76e8011abeb3f3025692b36fd
Olawale Olabisi <ejiolawale4@gmail.com>
2024-12-04 15:11:11 -0500

    test project

---
+++ WebAPI.Tests/Controllers/CityControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+using WebAPI.Dtos;
+using WebAPI.Interfaces;
+using WebAPI.Models;
+
+namespace WebAPI.Tests.Controllers
+{
+    public class CityControllerTests
+    {
+        private readonly Mock<IUnitOfWork> _mockUow;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly CityController _controller;
+
+        public CityControllerTests()
+        {
+            _mockUow = new Mock<IUnitOfWork>();
+            _mockMapper = new Mock<IMapper>();
+            _controller = new CityController(_mockUow.Object, _mockMapper.Object);
+        }
+
+        [Fact]
+        public async Task GetCities_ReturnsOkResult_WithListOfCities()
+        {
+            // Arrange
+            var cities = new List<City>
+            {
+                new City("City1", "Country1"),
+                new City("City2", "Country2")
+            };
+
+            _mockUow.Setup(u => u.CityRepository.GetCitiesAsync()).ReturnsAsync(cities);
+
+            _mockMapper.Setup(m => m.Map<IEnumerable<CityDto>>(cities))
+                       .Returns(new List<CityDto>
+                       {
+                           new CityDto { Id = 1, Name = "City1" },
+                           new CityDto { Id = 2, Name = "City2" }
+                       });
+
+            // Act
+            var result = await _controller.GetCities();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedCities = Assert.IsType<List<CityDto>>(okResult.Value);
+            Assert.Equal(2, returnedCities.Count);
+        }
+
+
+        [Fact]
+        public async Task UpdateCity_IdMismatch_ReturnsBadRequest()
+        {
+            // Arrange
+            var cityDto = new CityDto { Id = 2, Name = "Updated City" };
+
+            // Act
+            var result = await _controller.UpdateCity(1, cityDto);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Update not allowed", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task DeleteCity_ValidId_ReturnsOk()
+        {
+            // Arrange
+            _mockUow.Setup(u => u.CityRepository.DeleteCity(1));
+            _mockUow.Setup(u => u.SaveAsync()).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.DeleteCity(1);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(1, okResult.Value);
+            _mockUow.Verify(u => u.CityRepository.DeleteCity(1), Times.Once);
+            _mockUow.Verify(u => u.SaveAsync(), Times.Once);
+        }
+
+    }
+}
---
+++ WebAPI.Tests/Controllers/PropertyControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+using WebAPI.Dtos;
+using WebAPI.Interfaces;
+using WebAPI.Models;
+
+
+namespace WebAPI.Tests.Controllers
+{
+    public class PropertyControllerTests
+    {
+        private readonly Mock<IUnitOfWork> _mockUow;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IPhotoService> _mockPhotoService;
+        private readonly PropertyController _controller;
+
+        public PropertyControllerTests()
+        {
+            _mockUow = new Mock<IUnitOfWork>();
+            _mockMapper = new Mock<IMapper>();
+            _mockPhotoService = new Mock<IPhotoService>();
+
+            _controller = new PropertyController(_mockUow.Object, _mockMapper.Object, _mockPhotoService.Object);
+        }
+
+        [Fact]
+        public async Task GetPropertyList_ReturnsOkResult_WithPropertyList()
+        {
+            // Arrange
+            var properties = new List<Property> { new Property { Id = 1, Name = "Test Property" } };
+            _mockUow.Setup(u => u.PropertyRepository.GetPropertiesAsync(It.IsAny<int?>(), It.IsAny<int?>(), It.IsAny<string>()))
+        .ReturnsAsync(properties.AsEnumerable());
+            _mockMapper.Setup(m => m.Map<IEnumerable<PropertyListDto>>(It.IsAny<IEnumerable<Property>>()))
+                       .Returns(new List<PropertyListDto> { new PropertyListDto { Id = 1, Name = "Test Property" } });
+
+            // Act
+            var result = await _controller.GetPropertyList();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedProperties = Assert.IsType<List<PropertyListDto>>(okResult.Value);
+            Assert.Single(returnedProperties);
+        }
+
+        [Fact]
+        public async Task GetPropertyDetail_ReturnsOkResult_WithPropertyDetail()
+        {
+            // Arrange
+            var property = new Property { Id = 1, Name = "Test Property" };
+            _mockUow.Setup(u => u.PropertyRepository.GetPropertyDetailAsync(It.IsAny<int>()))
+                    .ReturnsAsync(property);
+            _mockMapper.Setup(m => m.Map<PropertyDetailDto>(It.IsAny<Property>()))
+                       .Returns(new PropertyDetailDto { Id = 1, Name = "Test Property" });
+
+            // Act
+            var result = await _controller.GetPropertyDetail(1);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedProperty = Assert.IsType<PropertyDetailDto>(okResult.Value);
+            Assert.Equal(1, returnedProperty.Id);
+        }
+
+
+
+    }
+}
